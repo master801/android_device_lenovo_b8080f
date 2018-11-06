3ffd0c3c145ef32e1ecf34fc1ef72ced1527e22f
master <thetruemaster801@gmail.com>
2018-11-05 19:12:57 -0600

    Fix cpu variant

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,7 +6,7 @@ TARGET_ARCH         := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI      := armeabi-v7a
 TARGET_CPU_ABI2     := armeabi
-TARGET_CPU_VARIANT  := cortex-a7
+TARGET_CPU_VARIANT  := krait
 
 # Platform
 TARGET_BOARD_PLATFORM     := msm8226
